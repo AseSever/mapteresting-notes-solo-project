66be1f2ed3ddd1a116f69670296aeff6479fd04f
Alex Severson <alexsvrsn@gmail.com>
2020-09-08 14:17:37 -0500

    added note post saga, updated pool to target database

--- src/redux/sagas/notes.saga.js
+++ src/redux/sagas/notes.saga.js
@@ -1,15 +1,19 @@
 import axios from 'axios';
 import { put, takeLatest } from 'redux-saga/effects';
 
-
+// post saga for sending notes to db
 function* sendNote(action) {
+  console.log(action.payload);
   try {
-    yield axios.post()
+    yield axios.post('/api/note', action.payload)
+
+  } catch ( err ) {
+    console.log('Error in sendNote POST saga', err);
   }
 }
 
 function* notesSaga() {
-    yield takeLatest();
+    yield takeLatest('SEND_NOTE', sendNote);
   }
   
   export default notesSaga;
